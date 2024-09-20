2e0cfba348ef19af4678e3772ea24490ea95b255
pbnguyen <pbnguyen@myseneca.ca>
2024-09-20 00:11:46 -0400

    Collecting details for solution

--- DIY02/Population.cpp
+++ DIY02/Population.cpp
@@ -1,8 +1,21 @@
 #include "Population.h"
+#include <cstdio>
+#include <iostream>
 
 using namespace std;
 namespace seneca {
 
+    bool load(const char filename[])
+    {
+        
+    }
 
+    void display()
+    {
+    }
+
+    void deallocateMemory()
+    {
+    }
 
 }
--- DIY02/Population.h
+++ DIY02/Population.h
@@ -2,6 +2,28 @@
 #define SENECA_POPULATION_H_
 namespace seneca {
 
+    struct Population {
+        char postalCode[4];
+        int numOfRec;
+    };
+
+    /// <summary>
+    /// Finds the number of records, allocates memory in the file scope pointer and then Loads the data from the file into the dynamically allocated population records, and returns true if data was successfully loaded, false otherwise
+    /// </summary>
+    /// <param name="filename"></param>
+    bool load(const char filename[]);
+
+
+    /// <summary>
+    /// a function with no parameters that returns nothing and prints the report as shown in the correct output
+    /// </summary>
+    void display();
+
+
+    /// <summary>
+    /// a function with no parameters that return nothing and deallocate all dynamic memory used by the program.
+    /// </summary>
+    void deallocateMemory();
 
 
 }
