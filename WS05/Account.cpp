5c6156eb60b3ce693307be309a37a0ed3db1eac8
pbnguyen <pbnguyen@myseneca.ca>
2024-10-07 23:34:40 -0400

    Still working on it, found some issues

--- WS05/Account.cpp
+++ WS05/Account.cpp
@@ -42,5 +42,109 @@ namespace seneca {
         return cout;
     }
 
+    Account::operator bool() const
+    {
+        return m_number != 0 && m_number != -1;
+    }
+
+    Account::operator int() const
+    {
+        return m_number;
+    }
+
+    Account::operator double() const
+    {
+        return m_balance;
+    }
+
+    bool Account::operator~()const
+    {
+        return m_number == 0;
+    }
+
+    Account& Account::operator=(int number)
+    {
+        if (!this->operator bool()) { //if account is in valid state
+            if (~*this) {            //if account is new
+                if (number > 10000 && number < 99999) { m_number = number; }
+                else { setEmpty(); }
+            }
+            
+        }
+        return *this;
+    }
+
+    Account& Account::operator=(Account& source)
+    {
+        if (!this->operator bool()) { //if account is in valid state
+            if (~*this || source.operator bool()) {            //if account is new or the right Account operand is valid
+                m_number = source.m_number;
+                m_balance = source.m_balance;
+                source.m_number = 0;
+                source.m_balance = 0;
+            }
+        }
+        return *this;
+    }
+
+    Account& Account::operator+=(double balance)
+    {
+        if (this->operator bool() && balance > 0.0) {     //valid state and positive number
+            m_balance += balance;
+        }
+        return *this;
+    }
+
+    Account& Account::operator-=(double number)
+    {
+        if (this->operator bool() && number > 0.0 && this->m_balance > number ) {     //valid state and positive number
+            m_balance -= number;
+        }
+        return *this;
+        return *this;
+    }
+
+    Account& Account::operator<<(Account& source)
+    {
+        if (this->operator bool() && source.operator bool() && this->m_number != source.m_number && source.m_balance > 0) {
+            //both Account must be in valid state and positive balance;
+            m_balance += source.m_balance;
+            source.m_balance = 0.0;
+        }
+        return *this;
+    }
+
+    Account& Account::operator>>(Account& source)
+    {
+        if (this->operator bool() && source.operator bool() && m_balance > 0.0 && source.m_balance > 0) {
+            //both Account must be in valid state and positive balance;
+            source.m_balance += m_balance;
+            m_balance = 0.0;
+        }
+        return *this;
+    }
+
+    
+    double operator+(const Account& left, const Account& right)
+    {
+        if (left.operator bool() && right.operator bool()) {
+            double sum{};
+            sum = left.operator double() + right.operator double();
+            return sum;
+        }
+        return 0;
+    }
+
+    double operator+=(double& number, const Account& right)
+    {
+        if (right.operator bool()) {
+            double sum{};
+            sum = number += right.operator double();    //number += right.operator double() actually modifies number by adding the value from right to it
+                                                        //The += operator updates number in place, and then sum stores the updated value of number 
+            return sum;
+        }
+        return 0;
+        
+    }
 
 }
--- WS05/Account.h
+++ WS05/Account.h
@@ -12,7 +12,79 @@ namespace seneca {
         Account(int number, double balance);
         std::ostream& display()const;
 
+        /// <summary>
+        /// returns true if the Account is valid and otherwise false. This operator can not modify the Account object.
+        /// </summary>
+        operator bool() const;
+
+        /// <summary>
+        /// returns the Account number. This operator can not modify the Account object.
+        /// </summary>
+        operator int() const;
+
+        /// <summary>
+        /// returns the balance value. This operator can not modify the Account object.
+        /// </summary>
+        operator double() const;
+
+        /// <summary>
+        /// This operator returns true if the Account is new or not set (i.e. if the Account number is zero)
+        /// otherwise it will return false. This operator can not modify the Account object.
+        /// </summary>
+        /// <returns></returns>
+        bool operator ~()const;
+
+        /// <summary>
+        /// set the Account number of the Account to the integer value.
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        Account& operator=(int number);
+
+        /// <summary>
+        /// overload the assignment operator so a NEW Account can be set to another Account object
+        /// This action should move the balance and the Account number from one Account to another
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        Account& operator=(Account& source);
+
+        /// <summary>
+        /// overload the += operator to add a double value to an Account. This should act like depositing money into an Account
+        /// </summary>
+        /// <param name="number2"></param>
+        /// <returns></returns>
+        Account& operator+=(double balance);
+
+        /// <summary>
+        /// overload the -= operator to reduce an Account balance by a double value.
+        /// This should act like withdrawing money from an Account
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        Account& operator-=(double number);
+
+        /// <summary>
+        /// sum of balance from both account
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        Account& operator<<(Account& source);
+
+        /// <summary>
+        /// same above
+        /// </summary>
+        /// <param name="source"></param>
+        /// <returns></returns>
+        Account& operator>>(Account& source);
+
     };
+
+
+    double operator+(const Account& left, const Account& right);
+
+
+    double operator+=(double& number , const Account& right);
 
 
 }
